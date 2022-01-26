c75c3374ea49556483ec942baa86832d2cb27527
Rabiaparlak <68352029+Rabiaparlak@users.noreply.github.com>
2022-01-26 19:59:50 +0300

    Update App.tsx
    views created

--- App.tsx
+++ App.tsx
@@ -12,16 +12,68 @@ export default function App() {
 
   const [name, setName] = React.useState("mehmet");
 
-
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+    <View>
+    <View style={{marginTop:70,borderBottomWidth:1 , borderColor:'grey',alignItems: 'center', justifyContent: 'center' }}>
+       <Text style={{color:'black', marginBottom:10}}>RM</Text>
+    </View>
+
+<View style={{flexDirection:"row"}}>
+
+
+
+    <View style={{marginLeft:20 }}>
       <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
       <TouchableOpacity
         onPress={() => { setName("Rabia") }}
-        style={{ borderRadius: 100, width: 180, height: 180, backgroundColor: 'blue', alignItems: 'center', justifyContent: 'center' }}
+        style={{ borderRadius: 20, width: 170, height: 180, backgroundColor: 'pink', alignItems: 'center', justifyContent: 'center' }}
       >
         <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
       </TouchableOpacity>
+
+    </View>
+    
+    <View style={{marginLeft:15 }}>
+      <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
+      <TouchableOpacity
+        onPress={() => { setName("Rabia") }}
+        style={{ borderRadius: 20, width: 170, height: 180, backgroundColor: 'pink', alignItems: 'center', justifyContent: 'center' }}
+      >
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
+      </TouchableOpacity>
+
+    </View>
+
+    </View>
+    
+<View style={{flexDirection:"row"}}>
+
+
+
+<View style={{marginLeft:20 }}>
+  <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
+  <TouchableOpacity
+    onPress={() => { setName("Rabia") }}
+    style={{ borderRadius: 20, width: 170, height: 180, backgroundColor: 'pink', alignItems: 'center', justifyContent: 'center' }}
+  >
+    <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
+  </TouchableOpacity>
+
+</View>
+
+<View style={{marginLeft:15 }}>
+  <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
+  <TouchableOpacity
+    onPress={() => { setName("Rabia") }}
+    style={{ borderRadius: 20, width: 170, height: 180, backgroundColor: 'pink', alignItems: 'center', justifyContent: 'center' }}
+  >
+    <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
+  </TouchableOpacity>
+
+</View>
+
+</View>
+
 
     </View>
   )
