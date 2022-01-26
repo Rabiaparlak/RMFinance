cb970817f2a6c9b68c67ce455aadfa10d299fd57
Rabiaparlak <68352029+Rabiaparlak@users.noreply.github.com>
2022-01-26 15:13:09 +0300

    Update App.tsx
    ...

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
+
+/*
+  const App = () => {
+
+  }
+  export default App;
+*/
 export default function App() {
+
+  const [name, setName] = React.useState("mehmet");
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+      <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
+      <TouchableOpacity
+        onPress={() => { setName("Rabia") }}
+        style={{ borderRadius: 100, width: 180, height: 180, backgroundColor: 'blue', alignItems: 'center', justifyContent: 'center' }}
+      >
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>{name}</Text>
+      </TouchableOpacity>
+
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
